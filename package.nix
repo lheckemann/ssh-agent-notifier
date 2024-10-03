1299656c97d8a8c72b753a16a538438b7955a29e
Linus Heckemann <git@sphalerite.org>
2024-10-03 13:02:21 +0200

    package: fix rebuild-avoiding source filter

--- package.nix
+++ package.nix
@@ -2,9 +2,17 @@
 rustPlatform.buildRustPackage {
   pname = "ssh-agent-notifier";
   inherit version;
-  src = nix-gitignore.gitignoreSource [
-    "*.nix"
-  ] ./.;
+  src = builtins.path {
+    name = "source";
+    path = ./.;
+    filter = nix-gitignore.gitignoreFilterPure (_: _: true) [
+      ./.gitignore
+      "*.nix"
+      ".gitignore"
+      "README.md"
+      ".envrc"
+    ] ./.;
+  };
   cargoLock = {
     lockFile = ./Cargo.lock;
     outputHashes."ssh-agent-lib-0.4.0" = "sha256-puYvOP9LiolcM460FJTBezBwwOkgW62I2UN8fe+Qb5k=";
